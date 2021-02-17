9996926f5b51f271b66999ace81e586ac8c19540
Samuel Avelino <48601223+samuelavelino@users.noreply.github.com>
2021-02-17 18:55:08 -0300

    Update helloworld.ps1

--- helloworld.ps1
+++ helloworld.ps1
@@ -1,1 +1,11 @@
+# Hello, World!
+$hello = "Hello, World!"
 
+Write-Output $hello
+
+<#
+If this is your first time running a Powershell (.ps1) script, you may have an issue
+running the script due to default resrictions. 
+Type this into the shell:
+Set-ExecutionPolicy RemoteSigned
+#>
